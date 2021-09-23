605f20d91638b333061663446e87ea58e83f7597
rayanubhab <rayanubhab@gmail.com>
2021-09-24 00:59:43 +0530

    Added exam section and updated signup login pages

---
+++ src/components/Exam/Question.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const Question = ({ data, onAnswerUpdate, numberOfQuestions, activeQuestion, onSetActiveQuestion, onSetStep }) => {
+  const [selected, setSelected] = useState('');
+  const [error, setError] = useState('');
+  const radiosWrapper = useRef();
+
+  useEffect(() => {
+    const findCheckedInput = radiosWrapper.current.querySelector('input:checked');
+    if(findCheckedInput) {
+      findCheckedInput.checked = false;
+    }
+  }, [data]);
+
+  const changeHandler = (e) => {
+    setSelected(e.target.value);
+    if(error) {
+      setError('');
+    }
+  }
+  
+  const nextClickHandler = (e) => {
+    if(selected === '') {
+      return setError('Please select one option!');
+    }
+    onAnswerUpdate(prevState => [...prevState, { q: data.question, a: selected }]);
+    setSelected('');
+    if(activeQuestion < numberOfQuestions - 1) {
+      onSetActiveQuestion(activeQuestion + 1);
+    }else {
+      onSetStep(3);
+    }
+  }
+
+  return(
+    <div className="card">
+      <div className="card-content">
+        <div className="content">
+          <h2 className="mb-5">{data.question}</h2>
+          <div className="player">
+              <audio id="player2" preload="none" controls controlsList="nodownload" style={{width:"100%",marginBottom:"10px"}}>
+                <source src="http://www.largesound.com/ashborytour/sound/AshboryBYU.mp3" type="audio/mp3"/>
+              </audio>
+            </div>
+          <div className="control" ref={radiosWrapper}>
+            {data.choices.map((choice, i) => (
+              <label className="radio has-background-light" key={i}>
+                <input type="radio" name="answer" value={choice} onChange={changeHandler} />
+                {choice}
+              </label>
+            ))}
+          </div>
+          {error && <div className="has-text-danger">{error}</div>}
+          <button className="button is-link is-medium is-fullwidth mt-4" onClick={nextClickHandler}>Next</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Question;
