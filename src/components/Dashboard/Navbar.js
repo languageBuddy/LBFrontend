7619f87d130638799afe7a632631305a0f416399
Shubham Thorat <thoratshubham048@gmail.com>
2021-09-28 01:40:11 +0530

    updated links

--- src/components/Dashboard/Navbar.js
+++ src/components/Dashboard/Navbar.js
@@ -44,8 +44,8 @@ const Navbar = ({ history }) => {
 
                     <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                         <li><Link to="/" className="nav-link px-2 text-secondary"> HOME </Link></li>
-                        <li><Link to="#" className="nav-link px-2 text-white">ABOUT</Link></li>
-                        <li><Link to="#" className="nav-link px-2 text-white"> EXAM</Link></li>
+                        <li><Link to="/dashboard/about" className="nav-link px-2 text-white">ABOUT</Link></li>
+                        <li><Link to="/dashboard/exam" className="nav-link px-2 text-white"> EXAM</Link></li>
                     </ul>
 
                     <Dropdown className="dropdown profile">
